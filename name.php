53ae09d534021fa51b9324ab8976283c6b9152ef
Arsh Butt <2009324@talnet.nl>
2020-05-28 12:48:40 +0200

    Uitwerking van de exercise

--- name.php
+++ name.php
@@ -1,46 +1,9 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-    <head>
-
-
-    </head>    
-
-
-<body>
-    
-    
-  <form method="post">
-    
-    <label for="naam">Wat is je naam?</label>
-    <input type="text" name="naam" required><br>
-    <label for="naam">Wat is je achternaam?</label>
-    <input type="text" name="achternaam" required><br>
-    <input type="submit" value="verzend" name="groet">
-    
-    
-  </form>
-        
-    
-  <?php
-        if(isset($_POST['naam']) && isset($_POST['achternaam']))
-        {
-            echo "Hallo, " . $_POST['naam'] . " " . $_POST['achternaam'] ;
-        }
-
- ?>  
-    
-    
-    
-    
-</body>
+    $voornaam = readline("wat is je voornaam? ");
+    $achternaam = readline("wat is je achternaam? ");
+    echo "Jouw naam is: " . $voornaam . " " . $achternaam;
 
 
 
-
-
-
-
-
-
-</html>
+?>
